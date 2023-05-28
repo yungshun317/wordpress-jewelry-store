bcd9d372e26a8c41e999e779405107ce8a61c30f
yungshun317 <seraph317@gmail.com>
2023-05-28 18:28:12 +0800

    [FEAT] Create page templates

--- template-home.php
+++ template-home.php
@@ -1,7 +1,15 @@
 <?php
+/*
+Template Name: Home Page
+ */
+
 get_header();
 ?>
 <main>
+    <section class="section-slider">Slider</section>
+    <section class="section-popular-products">Popular Products</section>
+    <section class="section-new-arrivals">New Arrival</section>
+    <section class="section-deal-of-the-week">Deal of the Week</section>
     <section class="section-blog">
         <?php
         if( have_posts() ):

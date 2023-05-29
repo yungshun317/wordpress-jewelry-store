1ba63f62a0ee60ae77b7c09340fc0fb1ec7542cd
yungshun317 <seraph317@gmail.com>
2023-05-29 21:39:41 +0800

    [FEAT] Declare WooCommerce support

--- functions.php
+++ functions.php
@@ -12,5 +12,25 @@ function online_jewelry_store_config() {
             'online_jewelry_store_footer_menu' => 'Online Jewelry Store Footer Menu'
         )
     );
+
+    add_theme_support( 'woocommerce', array(
+        'thumbnail_image_width' => 255,
+        'single_image_width' => 255,
+        'product_grid' => array(
+            'default_rows' => 10,
+            'min_rows' => 5,
+            'max_rows' => 10,
+            'default_columns' => 1,
+            'min_columns' => 1,
+            'max_columns' => 5
+        )
+    ));
+    add_theme_support( 'wc-product-gallery-zoom' );
+    add_theme_support( 'wc-product-gallery-lightbox' );
+    add_theme_support( 'wc-product-gallery-slider' );
+
+    if ( ! isset( $content_width ) ) {
+        $content_width = 600;
+    }
 }
 add_action( 'after_setup_theme', 'online_jewelry_store_config', 0 );

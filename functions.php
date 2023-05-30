403043810c40692fb922ac438cf142f4399b10ae
yungshun317 <seraph317@gmail.com>
2023-05-30 08:36:30 +0800

    [FEAT] Override WooCommerce templates using action hooks and filters

--- functions.php
+++ functions.php
@@ -34,3 +34,36 @@ function online_jewelry_store_config() {
     }
 }
 add_action( 'after_setup_theme', 'online_jewelry_store_config', 0 );
+
+add_action( 'woocommerce_before_main_content', 'online_jewelry_store_open_container_row', 5 );
+function online_jewelry_store_open_container_row() {
+    echo '<div class="container shop-content"><div class="row">';
+}
+
+add_action( 'woocommerce_after_main_content', 'online_jewelry_store_close_container_row', 5 );
+function online_jewelry_store_close_container_row() {
+    echo '</div></div>';
+}
+
+add_action( 'woocommerce_before_main_content', 'online_jewelry_store_add_sidebar_tags', 6 );
+function online_jewelry_store_add_sidebar_tags() {
+    echo '<div class="sidebar-shop col-lg-3 col-md-4 order-2 order-md-1">';
+}
+
+remove_action( 'woocommerce_sidebar', 'woocommerce_get_sidebar' );
+add_action( 'woocommerce_before_main_content', 'woocommerce_get_sidebar', 7 );
+
+add_action( 'woocommerce_before_main_content', 'online_jewelry_store_close_sidebar_tags', 8 );
+function online_jewelry_store_close_sidebar_tags() {
+    echo '</div>';
+}
+
+add_action( 'woocommerce_before_main_content', 'online_jewelry_store_add_shop_tags', 9 );
+function online_jewelry_store_add_shop_tags() {
+    echo '<div class="col-lg-9 col-md-8 col-1 order-md-2">';
+}
+
+add_action( 'woocommerce_after_main_content', 'online_jewelry_store_close_shop_tags', 4 );
+function online_jewelry_store_close_shop_tags() {
+    echo '</div>';
+}
